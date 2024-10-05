712f413ebc9fc70bca71b6276cab39a3a595c96a
Belinda Weng <bweng87@gmail.com>
2024-10-05 19:04:25 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="intro_box">
+      <h1>hello! i'm belinda</h1>
+      <p>welcome to my personal website!</p>
     </div>
   );
 }
